585b769bad6c270781027ee74225929f24b41c84
Noor Fatima <nf65027@gmail.com>
2026-04-21 14:43:46 +0500

    Props Practice

---
+++ PropsJSX/src/App.jsx
@@ -0,0 +1,27 @@
+
+import Card from "./Card"
+function App() {
+
+
+  return (
+    <>
+    <Card>
+      <h1>Myself</h1>
+      <p>My name is Noor Fatima. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, optio.</p>
+      <button>
+        Read More
+      </button>
+    </Card>
+    <Card>
+      <h1>Hobby</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus at aspernatur temporibus aliquam non necessitatibus fuga officiis quos, earum laboriosam a cumque, expedita eum fugiat.</p>
+      <button>
+        Read More
+      </button>
+    </Card>
+    </>
+    
+  )
+}
+
+export default App
